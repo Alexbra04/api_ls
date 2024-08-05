c59ba565ce7aef60c7510e93efd0a720aa3b3d75
BRYAN ALEXANDER FIGUEROA ALOMIA <bryan.figueroa1979@utc.edu.ec>
2024-08-04 20:53:34 -0500

    Entrenamiento abecedario

--- abecedario/abecedario_api.py
+++ abecedario/abecedario_api.py
@@ -122,16 +122,18 @@ def procesar_gesto(hand_landmarks, image):
         thumb_tip[1] - thumb_pip[1] < 10):
         return 'I'
     elif (index_finger_tip[1] < thumb_tip[1] and
-          middle_finger_tip[1] < thumb_tip[1] and
-          ring_finger_tip[1] < thumb_tip[1] and
-          pinky_tip[1] < thumb_tip[1] and
-          thumb_tip[0] < index_finger_tip[0] and
-          thumb_tip[0] < middle_finger_tip[0] and
-          thumb_tip[0] < ring_finger_tip[0] and
-          thumb_tip[0] < pinky_tip[0] and
-          abs(index_finger_tip[0] - middle_finger_tip[0]) < 50 and
-          abs(index_finger_tip[0] - ring_finger_tip[0]) < 50 and
-          abs(index_finger_tip[0] - pinky_tip[0]) < 50):
+        middle_finger_tip[1] < thumb_tip[1] and
+        ring_finger_tip[1] < thumb_tip[1] and
+        pinky_tip[1] < thumb_tip[1] and
+        index_finger_tip[1] < middle_finger_tip[1] and
+        index_finger_tip[1] < ring_finger_tip[1] and
+        index_finger_tip[1] < pinky_tip[1] and
+        middle_finger_tip[1] < ring_finger_tip[1] and
+        middle_finger_tip[1] < pinky_tip[1] and
+        abs(thumb_tip[0] - index_finger_tip[0]) < 50 and
+        abs(thumb_tip[0] - middle_finger_tip[0]) < 50 and
+        abs(thumb_tip[0] - ring_finger_tip[0]) < 50 and
+        abs(thumb_tip[0] - pinky_tip[0]) < 50):
         return 'R'
 
 # Ruta para detectar gestos
