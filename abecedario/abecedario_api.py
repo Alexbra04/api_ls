07ae860d3c670da1ac609c8d370f3025aabf2129
BRYAN ALEXANDER FIGUEROA ALOMIA <bryan.figueroa1979@utc.edu.ec>
2024-08-04 20:12:02 -0500

    Entrenamiento abecedario

--- abecedario/abecedario_api.py
+++ abecedario/abecedario_api.py
@@ -118,7 +118,10 @@ def procesar_gesto(hand_landmarks, image):
           thumb_tip[1] < index_finger_pip[1] and 
           thumb_tip[1] < middle_finger_pip[1] and 
           thumb_tip[1] < ring_finger_pip[1] and 
-          abs(thumb_tip[0] - pinky_tip[0]) < 40):
+          abs(thumb_tip[0] - pinky_tip[0]) < 40 and 
+          pinky_tip[1] < index_finger_tip[1] and 
+          pinky_tip[1] < middle_finger_tip[1] and 
+          pinky_tip[1] < ring_finger_tip[1]):
         return 'I'
 
 # Ruta para detectar gestos
