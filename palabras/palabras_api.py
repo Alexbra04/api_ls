3983b530ad63bb37c02e9ff91a12a8b8d393d705
Alexbra04 <134638550+Alexbra04@users.noreply.github.com>
2024-08-18 12:48:52 -0500

    Dos manos

--- palabras/palabras_api.py
+++ palabras/palabras_api.py
@@ -15,7 +15,7 @@ mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(
     static_image_mode=False,
-    max_num_hands=1,
+    max_num_hands=2,
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5)
 
